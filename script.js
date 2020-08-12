7d48492dc38a3e8817a36a7204e2b47c7de8ff5d
VighneshManjrekar <69366251+VighneshManjrekar@users.noreply.github.com>
2020-08-12 23:14:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,36 @@
+var cwin = 0;
 var uwin = 0;
-    var cwin = 0;
-    var r = document.getElementById('r').addEventListener('click', function () {
-        newmove('r');
-    });
-    var p = document.getElementById('p').addEventListener('click', function () {
-        newmove('p');
-    });
-    var s = document.getElementById('s').addEventListener('click', function () {
-        newmove('s');
-    });
-    function newmove(uc) {
-        console.log(uc);
+function ch(uc){
+    var uc = uc.value;
+    console.log(uc);
+    var cc = rancc();    
+    console.log(cc);
+    
+    if(uc=='r' && cc=='s' || uc=='p' && cc=='r' || uc=='s' && cc=='p' ){
+        document.getElementById('result').innerText ='You Won';
+        win()
+;        
+    }else if(uc==cc){
+        document.getElementById('result').innerText ='Draw';
+    }else{
+        document.getElementById('result').innerText ='You lost';
+        lose();
     }
+    document.getElementById('user-choice').src="images/"+uc+".png";
+    document.getElementById('comp-choice').src="images/"+cc+".png";
+    document.getElementById('score').innerHTML = "USer : " +uwin + "&nbsp;&nbsp; " + "Comp : " + cwin;
+}
 
-    function getcc() {
-        const ch = ['r', 'p', 's'];
-        var rancc = ch[Math.floor(Math.random() * 3)];
-        return rancc;
+function win(){
+    uwin++;
+}
 
+function lose(){
+    cwin++;
+}
 
-    }
-    function newmove(uc) {
-        var cc = getcc();
-        switch (uc + cc) {
-            case 'rs':
-            case 'pr':
-            case 'sp':
-                document.getElementById('result').innerHTML = 'User Wins';
-                uwin++;
-                break;
-            case 'rp':
-            case 'sr':
-            case 'ps':
-                document.getElementById('result').innerHTML = 'User Lose';
-                cwin++;
-                break;
-            case 'rr':
-            case 'pp':
-            case 'ss':
-                document.getElementById('result').innerHTML = 'User Draw';
-                break;
-        }
-        console.log(cc);
-        console.log(uc);
-        document.getElementById('score').innerHTML = "User : " + uwin +"&nbsp;&nbsp;"+ " Comp : " + cwin;
-
-        document.getElementById("user-image").src = "images/" + uc + ".png";
-        document.getElementById("comp-image").src = "images/" + cc + ".png";
-    }   
+function rancc(){
+     var choice = ['r','p','s','r','p','s'];
+     var compc = choice[Math.floor(Math.random()*6)];
+     return compc;
+}
